26cb6465becb758a5c4872f811b17e2a8e4d191d
den_m <denismartyanov244@gmail.com>
2025-02-24 17:59:02 +0200

    added env read

--- FrontService/src/main/java/ru/spshop/FrontService.java
+++ FrontService/src/main/java/ru/spshop/FrontService.java
@@ -1,11 +1,19 @@
 package ru.spshop;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FrontService {
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+			System.out.println("Loaded: " + entry.getKey() + " = " + entry.getValue());
+		});
+		System.out.println("SERVER_PORT from System.getProperty(): " + System.getProperty("SERVER_PORT"));
+
 		SpringApplication.run(FrontService.class, args);
 	}
 
